d4860c36614cd5eefe3c6e559e1c0c71c26d2bed
Germey <cqc@cuiqingcai.com>
2020-02-29 22:40:13 +0800

    add cetic

--- postgresql/run.sh
+++ postgresql/run.sh
@@ -1,1 +1,8 @@
 helm install --name postgresql --namespace postgresql bitnami/postgresql -f values-production.yaml --set postgresqlPassword=$PGSQL_PASSWORD --set replication.password=$PGSQL_PASSWORD
+
+
+
+
+# ok
+helm repo add cetic https://cetic.github.io/helm-charts
+helm install --name postgresql --namespace postgresql -f values-production.yml cetic/postgresql
